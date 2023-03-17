45ca2c4df59bc0ee332a94350058138957538a39
Builab <huy.bui@mcgill.ca>
2023-03-17 11:03:07 -0400

    Update fit_all_domain_in_chimerax.py

--- fit_all_domain_in_chimerax.py
+++ fit_all_domain_in_chimerax.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-# Python open a map
+# Usage
+# python fit_all_domain_in_chimerax pdb_dir output_dir input_map map_level resolution search
 
 import sys,os,time
 from datetime import datetime
@@ -10,15 +11,16 @@ import subprocess
 pdb_dir = sys.argv[1]
 output_dir = sys.argv[2]
 input_map = sys.argv[3]
-resolution = sys.argv[4]
-search = sys.argv[5]
+map_level = sys.argv[4]
+resolution = sys.argv[5]
+search = sys.argv[6]
 
 list = os.listdir(pdb_dir)
 cmds=[]
 for pdb in os.listdir(pdb_dir):
     if pdb.endswith(".pdb"):
         # Add them to the command list
-        cmds.append(f'chimerax-daily --nogui --offscreen --cmd \"runscript {script_dir}/fit_in_chimerax.py {pdb_dir}/{pdb} {output_dir} {input_map} {resolution} {search}" --exit')
+        cmds.append(f'chimerax-daily --nogui --offscreen --cmd \"runscript {script_dir}/fit_in_chimerax.py {pdb_dir}/{pdb} {output_dir} {input_map} {map_level} {resolution} {search}" --exit')
 
 # Execute command list
 #os.chdir(output_dir)
