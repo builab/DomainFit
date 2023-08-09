f5c23b840961ffcbaff51aac1622094be758cd5e
builab <huy.bui@mcgill.ca>
2023-08-09 05:22:13 -0400

    Update source_env.sh

--- source_env.sh
+++ source_env.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-# Get the directory of the current script
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+# Get the directory of the current script (Not working in Mac)
+CHIMERAX_DOMAIN_FIT="/Users/kbui2/Documents/GitHub/chimerax_domain_fit"
+AFSCRIPT=/Users/kbui2/Documents/GitHub/chimerax_domain_fit/AlphaFold
+#AFSCRIPT=$CHIMERAX_DOMAIN_FIT/AlphaFold
 # Add script directory and its subdirectory to the PATH
-export PATH="$SCRIPT_DIR:$SCRIPT_DIR/AlphaFold:$PATH"
+export PATH=$PATH:$CHIMERAX_DOMAIN_FIT
+#export PATH=$PATH:$AFSCRIPT
